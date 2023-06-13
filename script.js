91046c578b33560d1000493f39cd3aca70191495
Snoopmallymal <119768934+Snoopmallymal@users.noreply.github.com>
2023-06-13 18:22:35 +0000

    Test 1

---
+++ script.js
@@ -0,0 +1,14 @@
+function login() {
+    let key = "gamesplz"
+    let pass = window.prompt("Enter Password")
+    if (password=="iwantgames") {
+        sessionStorage.setItem("auth", "true");
+        window.open("launcher.html", "_top", "", "true");
+    }
+    else if (1 = 3) {
+
+    }
+    else {
+        window.open("google.com", "_top", "", "true");
+      }
+}
