4b4c8e5fd12a3882bf2874b99b4c286042e77b0f
Snoopmallymal <119768934+Snoopmallymal@users.noreply.github.com>
2023-06-13 21:31:04 +0000

    House Work

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function login() {
-    let key = "gamesplz"
-    let pass = window.prompt("Enter Password")
+    let key = "gamesplz";
+    let pass = window.prompt("Enter Password");
     if (password=="iwantgames") {
         sessionStorage.setItem("auth", "true");
         window.open("launcher.html", "_top", "", "true");
     }
     else if (1 = 3) {
-
+        window.alert("delete me");
     }
     else {
         window.open("google.com", "_top", "", "true");
